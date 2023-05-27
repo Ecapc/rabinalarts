9673c3d4e40e91d657f86bb019e18c4e0f1244bf
Emerson Andres Perez Chen <emersonandresperezchen@PcEmerson.local>
2023-05-26 18:09:51 -0600

    updatesubcategories

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -1,6 +1,6 @@
 <style>
-    #navigation-menu{
-        height: calc(100vh - 4rem );
+    #navigation-menu {
+        height: calc(100vh - 4rem);
     }
 </style>
 
@@ -20,115 +20,118 @@
             <x-application-mark class="block h-9 w-auto" />
         </a>
         @livewire('search')
-        
+
         <div>
             @auth
-            <div class="hidden sm:flex sm:items-center sm:ml-6">
-                <!-- Teams Dropdown -->
-                @if (Laravel\Jetstream\Jetstream::hasTeamFeatures())
-                    <div class="ml-3 relative">
-                        <x-dropdown align="right" width="60">
-    
-                            <x-slot name="content">
-                                <div class="w-60">
-                                    <!-- Team Management -->
-                                    <div class="block px-4 py-2 text-xs text-gray-400">
-                                        {{ __('Manage Team') }}
-                                    </div>
-    
-                                    <!-- Team Settings -->
-                                    <x-dropdown-link href="{{ route('teams.show', Auth::user()->currentTeam->id) }}">
-                                        {{ __('Team Settings') }}
-                                    </x-dropdown-link>
-    
-                                    @can('create', Laravel\Jetstream\Jetstream::newTeamModel())
-                                        <x-dropdown-link href="{{ route('teams.create') }}">
-                                            {{ __('Create New Team') }}
-                                        </x-dropdown-link>
-                                    @endcan
-    
-                                    <!-- Team Switcher -->
-                                    @if (Auth::user()->allTeams()->count() > 1)
-                                        <div class="border-t border-gray-200 dark:border-gray-600"></div>
-    
+                <div class="hidden sm:flex sm:items-center sm:ml-6">
+                    <!-- Teams Dropdown -->
+                    @if (Laravel\Jetstream\Jetstream::hasTeamFeatures())
+                        <div class="ml-3 relative">
+                            <x-dropdown align="right" width="60">
+
+                                <x-slot name="content">
+                                    <div class="w-60">
+                                        <!-- Team Management -->
                                         <div class="block px-4 py-2 text-xs text-gray-400">
-                                            {{ __('Switch Teams') }}
+                                            {{ __('Manage Team') }}
                                         </div>
-    
-                                        @foreach (Auth::user()->allTeams() as $team)
-                                            <x-switchable-team :team="$team" />
-                                        @endforeach
-                                    @endif
+
+                                        <!-- Team Settings -->
+                                        <x-dropdown-link href="{{ route('teams.show', Auth::user()->currentTeam->id) }}">
+                                            {{ __('Team Settings') }}
+                                        </x-dropdown-link>
+
+                                        @can('create', Laravel\Jetstream\Jetstream::newTeamModel())
+                                            <x-dropdown-link href="{{ route('teams.create') }}">
+                                                {{ __('Create New Team') }}
+                                            </x-dropdown-link>
+                                        @endcan
+
+                                        <!-- Team Switcher -->
+                                        @if (Auth::user()->allTeams()->count() > 1)
+                                            <div class="border-t border-gray-200 dark:border-gray-600"></div>
+
+                                            <div class="block px-4 py-2 text-xs text-gray-400">
+                                                {{ __('Switch Teams') }}
+                                            </div>
+
+                                            @foreach (Auth::user()->allTeams() as $team)
+                                                <x-switchable-team :team="$team" />
+                                            @endforeach
+                                        @endif
+                                    </div>
+                                </x-slot>
+                            </x-dropdown>
+                        </div>
+                    @endif
+
+                    <!-- Settings Dropdown -->
+                    <div class="ml-3 relative">
+                        <x-dropdown align="right" width="48">
+                            <x-slot name="trigger">
+                                @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
+                                    <button
+                                        class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
+                                        <img class="h-8 w-8 rounded-full object-cover"
+                                            src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
+                                    </button>
+                                @else
+                                    <span class="inline-flex rounded-md">
+                                        <button type="button"
+                                            class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none focus:bg-gray-50 dark:focus:bg-gray-700 active:bg-gray-50 dark:active:bg-gray-700 transition ease-in-out duration-150">
+                                            {{ Auth::user()->name }}
+
+                                            <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg"
+                                                fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                                                <path stroke-linecap="round" stroke-linejoin="round"
+                                                    d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                                            </svg>
+                                        </button>
+                                    </span>
+                                @endif
+                            </x-slot>
+
+                            <x-slot name="content">
+                                <!-- Account Management -->
+                                <div class="block px-4 py-2 text-xs text-gray-400">
+                                    {{ __('Manage Account') }}
                                 </div>
+
+                                <x-dropdown-link href="{{ route('profile.show') }}">
+                                    {{ __('Profile') }}
+                                </x-dropdown-link>
+
+                                <div class="border-t border-gray-200 dark:border-gray-600"></div>
+
+                                <!-- Authentication -->
+                                <form method="POST" action="{{ route('logout') }}" x-data>
+                                    @csrf
+
+                                    <x-dropdown-link href="{{ route('logout') }}" @click.prevent="$root.submit();">
+                                        {{ __('Log Out') }}
+                                    </x-dropdown-link>
+                                </form>
                             </x-slot>
                         </x-dropdown>
                     </div>
-                @endif
-    
-                <!-- Settings Dropdown -->
-                <div class="ml-3 relative">
-                    <x-dropdown align="right" width="48">
-                        <x-slot name="trigger">
-                            @if (Laravel\Jetstream\Jetstream::managesProfilePhotos())
-                                <button class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
-                                    <img class="h-8 w-8 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
-                                </button>
-                            @else
-                                <span class="inline-flex rounded-md">
-                                    <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none focus:bg-gray-50 dark:focus:bg-gray-700 active:bg-gray-50 dark:active:bg-gray-700 transition ease-in-out duration-150">
-                                        {{ Auth::user()->name }}
-    
-                                        <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                                            <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
-                                        </svg>
-                                    </button>
-                                </span>
-                            @endif
-                        </x-slot>
-    
-                        <x-slot name="content">
-                            <!-- Account Management -->
-                            <div class="block px-4 py-2 text-xs text-gray-400">
-                                {{ __('Manage Account') }}
-                            </div>
-    
-                            <x-dropdown-link href="{{ route('profile.show') }}">
-                                {{ __('Profile') }}
-                            </x-dropdown-link>
-    
-                            <div class="border-t border-gray-200 dark:border-gray-600"></div>
-    
-                            <!-- Authentication -->
-                            <form method="POST" action="{{ route('logout') }}" x-data>
-                                @csrf
-    
-                                <x-dropdown-link href="{{ route('logout') }}"
-                                         @click.prevent="$root.submit();">
-                                    {{ __('Log Out') }}
-                                </x-dropdown-link>
-                            </form>
-                        </x-slot>
-                    </x-dropdown>
                 </div>
-            </div>
-
             @else
-            <x-dropdown align="right" width="48">
-                <x-slot name="trigger">
-                    <i class="fas fa-user-circle text-white text-3xl cursor-pointer items-center justify-center px-4">
-                    </i>
-                </x-slot>
+                <x-dropdown align="right" width="48">
+                    <x-slot name="trigger">
+                        <i class="fas fa-user-circle text-white text-3xl cursor-pointer items-center justify-center px-4">
+                        </i>
+                    </x-slot>
 
-                <x-slot name="content">
-                    <x-dropdown-link href="{{ route('login') }}">
-                        {{ __('Login') }}
-                    </x-dropdown-link>
+                    <x-slot name="content">
+                        <x-dropdown-link href="{{ route('login') }}">
+                            {{ __('Login') }}
+                        </x-dropdown-link>
 
-                    <x-dropdown-link href="{{ route('register') }}">
-                        {{ __('Register') }}
-                    </x-dropdown-link>
-                </x-slot>
-            </x-dropdown>
+                        <x-dropdown-link href="{{ route('register') }}">
+                            {{ __('Register') }}
+                        </x-dropdown-link>
+                    </x-slot>
+                </x-dropdown>
             @endauth
         </div>
 
@@ -137,27 +140,48 @@
     </div>
     <nav id="navigation-menu" class="bg-blueGray-400 bg-opacity-25 w-full absolute">
         <div class="container h-full">
-            <div class="grid grid-cols-4 h-full"> 
+            <div class="grid grid-cols-4 h-full relative">
                 <ul class="bg-gray-300 ">
                     @foreach ($categories as $category)
-                        <li class="text-blueGray-500 hover:bg-blueGray-600 hover:text-white">
+                        <li class="text-blueGray-700 hover:bg-blueGray-600 hover:text-white"> <!-- texto de subcategorias -->
                             <a href=" " class="py-2 px-4 text-sm flex items-center">
-                                <span>
-                                    {!!$category->icon!!}
+                                <span class="flex justify-center w-9">
+                                    {!! $category->icon !!}
                                 </span>
-                                {{$category->name }}
+                                {{ $category->name }}
                             </a>
-                        </li> 
+
+                            <div class="bg-yellow-500 absolute w-3/4 top-0 h-full right-0 hidden">
+                            </div>
+                        </li>
                     @endforeach
                 </ul>
 
                 <div class="col-span-3 bg-blueGray-500">
+                    <div class="grid grid-cols-4">
+                        <div>
+                            <p class="text-lg font-bold text-center text-blueGray-900 mb-3"> 
+                                Subcategorias
+                            </p>
 
+                            <ul> 
+                                @foreach ($categories->first()->subcategories as $subcategory)
+                                    <li>
+                                        <a href=" " class="text-blueGray-900 font-semibold inline-block py-1 px-4 hover:bg-blueGray-600 hover:text-white">
+                                            {{$subcategory->name}}
+                                        </a>
+                                    </li>
+                                @endforeach
+                            </ul>
+                        </div>
+
+                        <div class="col-span-3">
+                            <img class="h-94 w-full object-cover object-center" src="{{Storage::url($categories->first()->image)}}" alt="">
+                        </div>
+                    </div>
                 </div>
             </div>
 
         </div>
     </nav>
 </header>
-
-   
